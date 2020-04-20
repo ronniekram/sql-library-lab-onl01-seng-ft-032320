415a2309eb19628124919243037191efa1313813
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-20 18:27:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,52 +1,49 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year
-  FROM books
-  JOIN series
-  ON series.id = books.series_id
-  WHERE series.id = 1 ORDER BY books.year;"
-end
+    "SELECT books.title, books.year
+    FROM books
+    WHERE books.series_id = 1
+    ORDER BY books.year"
+  end
+  
+  def select_name_and_motto_of_char_with_longest_motto
+    "SELECT characters.name, characters.motto
+    FROM characters
+    ORDER BY LENGTH(characters.motto)
+    DESC LIMIT 1"
+  end
 
-def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, characters.motto 
-  FROM characters 
-  ORDER BY LENGTH(characters.motto) DESC LIMIT 1;"
-end
-
-def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species)
-  FROM characters
-  GROUP BY species
-  ORDER BY species DESC LIMIT 1;"
-end
-
-def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name
-  FROM authors
-  JOIN series
-  ON authors.id = series.author_id
-  JOIN subgenres 
-  ON subgenres.id = series.subgenre_id
-  ORDER BY authors.id;"
-end
-
-def select_series_title_with_most_human_characters
-  "SELECT series.title 
-  FROM series 
-  INNER JOIN books 
-  ON series.id = books.series_id 
-  JOIN character_books 
-  ON books.id = character_books.book_id 
-  JOIN characters 
-  ON characters.id = character_books.character_id
-  GROUP BY series.title HAVING COUNT(characters.species = 'human') LIMIT 1;"
-end
-
-def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(books.title) 
-  FROM characters
-  INNER JOIN character_books
-  ON characters.id = character_books.character_id
-  JOIN books 
-  ON books.id = character_books.book_id
-  GROUP BY characters.name ORDER BY COUNT(books.title) DESC;"
-end
+  def select_value_and_count_of_most_prolific_species
+    "SELECT characters.species, COUNT(characters.species)
+    FROM characters
+    GROUP BY characters.species
+    ORDER BY COUNT(characters.species)
+    DESC LIMIT 1"
+  end
+  
+  def select_name_and_series_subgenres_of_authors
+    "SELECT authors.name, subgenres.name
+    FROM authors
+    JOIN series
+    ON authors.id = series.author_id
+    JOIN subgenres
+    ON subgenres.id = series.subgenre_id
+    GROUP BY authors.name"
+  end
+  
+  def select_series_title_with_most_human_characters
+    "SELECT series.title
+    FROM series
+    JOIN authors ON authors.id = series.author_id
+    JOIN characters ON authors.id = characters.author_id
+    WHERE characters.species = 'human'
+    GROUP BY series.title LIMIT 1"
+  end
+  
+  def select_character_names_and_number_of_books_they_are_in
+    "SELECT characters.name, COUNT(books.title)
+    FROM characters
+    JOIN character_books ON characters.id = character_books.character_id
+    JOIN books ON books.id = character_books.book_id
+    GROUP BY characters.name 
+    ORDER BY COUNT(books.title) DESC"
+  end
